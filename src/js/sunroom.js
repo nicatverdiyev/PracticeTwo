3a609c92ae94e97e2f60397a306c96c392f52365
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-01-24 14:56:56 +0400

    sunroom page added to proje

---
+++ src/js/sunroom.js
@@ -0,0 +1,74 @@
+let products = [
+    {
+        "id":1,
+        "title":"Villa with garden",
+        "place":"Trani - Italy",
+        "brand":"Sloped Pergolas, Glass Walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+    },
+    {
+        "id":2,
+        "title":"Contemporary house with swimming pool",
+        "place":"Portorose - Slovenia",
+        "brand":"Bioclimatic Pergolas",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":3,
+        "title":"Family house with garden",
+        "place":"Antibes - France",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":4,
+        "title":"Villa with garden",
+        "place":"Trani - Italy",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":5,
+        "title":"Family house with garden",
+        "place":"Antibes - France",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":6,
+        "title":"Contemporary house with swimming pool",
+        "place":"Slovenia",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":7,
+        "title":"Villa with garden",
+        "place":"Trani - Italy",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":8,
+        "title":"Contemporary house with swimming pool",
+        "place":"Portorose - Slovenia",
+        "brand":"Bioclimatic Pergolas",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    },
+    {
+        "id":9,
+        "title":"Family house with garden",
+        "place":"Antibes - France",
+        "brand":"Sloped Pergolas, Glass walls",
+        "thumbnail":"https://duzce.ktb.gov.tr/Resim/427807,ucunculuk-odulumert-oktayjpg.png?0"
+
+    }
+    
+]
