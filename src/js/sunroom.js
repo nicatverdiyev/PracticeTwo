c266b9d7eac11c24bca755e312502c40503ac6d6
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-01-26 12:52:35 +0400

    button added to sunroom page

--- src/js/sunroom.js
+++ src/js/sunroom.js
@@ -177,7 +177,24 @@ products.forEach(item => productsElem.innerHTML += `
                       </defs>
                     </svg>
                     <p>${item.brand}</p>
+
                   </div>
+                  <div class="buttonJs">
+                  <button><a href="">Find Out More</a>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+  <g clip-path="url(#clip0_72_10959)">
+    <path d="M9.78355 6.5576L3.84115 12.5L2.22115 10.88L6.59875 6.5L2.21875 2.12L3.84115 0.5L9.78355 6.4424L9.72595 6.5L9.78355 6.5576Z" fill="#333333"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_72_10959">
+      <rect width="12" height="12" fill="white" transform="translate(0 0.5)"/>
+    </clipPath>
+  </defs>
+</svg>
+                  </button>
+                  
+                </div>
+
                 </div>
               </div>
             </div>
@@ -186,3 +203,135 @@ products.forEach(item => productsElem.innerHTML += `
 `
     
     );
+  // -----FİLTERED FUNCTİON----START---
+    const searchElem = document.getElementById("searchProduc");
+    searchElem.addEventListener("keyup", ()=>{
+      let filteredProduc = products.filter(item=> item.title.toLowerCase().includes(searchElem.value.toLocaleLowerCase()))
+
+      productsElem.innerHTML= "";
+      filteredProduc.forEach(item => productsElem.innerHTML+=`
+
+      <div class="col-lg-4">
+      <div class="villagesBox">
+        <img
+          src="${item.thumbnail}"
+          alt=""
+        />
+        <div class="villagesBoxText">
+          <h3>${item.title}</h3>
+          <div class="villagesBoxTextOne">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_72_10940)">
+                <path
+                  d="M12 12.8572C15.3137 12.8572 18 10.1709 18 6.85718C18 3.54347 15.3137 0.857178 12 0.857178C8.68629 0.857178 6 3.54347 6 6.85718C6 10.1709 8.68629 12.8572 12 12.8572Z"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12 12.8572V23.1429"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M9.42773 23.1429H14.5706"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_72_10940">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <p>${item.place}</p>
+          </div>
+          <div class="villagesBoxTextTwo">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_72_10947)">
+                <path
+                  d="M14.5717 23.1429H0.857422V6.85718L7.71456 0.857178L14.5717 6.85718V23.1429Z"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M14.5723 23.1429H23.1437V11.1429H14.5723"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M7.71484 23.1429V19.7144"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M5.14258 14.5714H10.2854"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M5.14258 9.42859H10.2854"
+                  stroke="#000001"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_72_10947">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <p>${item.brand}</p>
+            
+          </div>
+          <div class="buttonJs">
+          <button><a href="">Find Out More</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+<g clip-path="url(#clip0_72_10959)">
+<path d="M9.78355 6.5576L3.84115 12.5L2.22115 10.88L6.59875 6.5L2.21875 2.12L3.84115 0.5L9.78355 6.4424L9.72595 6.5L9.78355 6.5576Z" fill="#333333"/>
+</g>
+<defs>
+<clipPath id="clip0_72_10959">
+<rect width="12" height="12" fill="white" transform="translate(0 0.5)"/>
+</clipPath>
+</defs>
+</svg>
+          </button>
+          
+        </div>
+
+        </div>
+      </div>
+    </div>
+      `)
+    });
+  // -----FİLTERED FUNCTİON----END---
+
